315351efa11f25496062c5830376561ccaec79fb
Suwat Chitchaoren <139094155+watza2809@users.noreply.github.com>
2023-07-10 10:37:33 +0700

    Add files via upload

---
+++ Hello.cpp
@@ -0,0 +1,21 @@
+#include <iostream>   //input output
+using namespace std; //standard
+
+int main()           //int or void การคืนรูปแบบของข้อมูลที่ส่งค่าคอมไฟเลอร์
+{
+
+	cout<<"Student Code : 66-060216-2023-6"<<endl;
+	cout<<"Student Name : Suwat Chitchaoren"<<endl;
+	cout<<"    Division : Information Technology"<<endl;
+	cout<<"        Year : 1 "<<endl;
+	cout<<"        Room : RA"<<endl;
+	cout<<"          Sex: Male"<<endl;
+	cout<<"          Age: 18"<<endl;
+	
+
+
+
+
+	system("pause");
+	return (0);      //ส่งค่า0กลับcompiler
+}
